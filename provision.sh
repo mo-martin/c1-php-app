030e61067e7032587bd385a8b3924740985ca5d0
Ivan <ivan.dechovsky@skybettingandgaming.com>
2016-10-19 10:45:52 +0100

    installing ppa

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 # #!/bin/bash
 
-Installing Docker
+#Installing Docker
 sudo apt-get update
 sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
 sudo apt-get install -y apt-transport-https ca-certificates
@@ -8,11 +8,11 @@ sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58
 sudo tee /etc/apt/sources.list.d/docker.list <<EOF
 deb https://apt.dockerproject.org/repo ubuntu-xenial main
 EOF
+
 sudo apt-get update
 sudo apt-get purge lxc-docker
 apt-cache policy docker-engine
 sudo apt-get install -y docker-engine curl python-pip
-
 sudo curl -L -s https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose -version
@@ -20,10 +20,10 @@ docker-compose -version
 #Add the User group
 sudo usermod -aG docker ubuntu
 
+#Installing Apache 
+sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
-
 sudo apt-get update
-
 sudo apt-get install php7.0 php5.6 php5.6-mysql php-gettext php5.6-mbstring php-xdebug libapache2-mod-php5.6 libapache2-mod-php7.0
 sudo a2dismod php7.0
 sudo a2enmod php5.6
